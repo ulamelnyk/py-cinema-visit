aa45f7df9170caee735588868a796f09b383d673
ulianamelnyk <melnykula23@gmail.com>
2026-07-08 15:38:49 -0400

    Solution

--- app/cinema/bar.py
+++ app/cinema/bar.py
@@ -1,5 +1,5 @@
 class CinemaBar:
 
     @staticmethod
-    def sell_product(product, customer):
+    def sell_product(product: str, customer: Customer) -> None:
         print(f"Cinema bar sold {product} to {customer.name}.")
--- app/people/customer.py
+++ app/people/customer.py
@@ -4,5 +4,5 @@ class Customer:
         self.name = name
         self.food = food
 
-    def watch_movie(self, movie):
+    def watch_movie(self, movie: str) -> None:
         print(f'{self.name} is watching "{movie}".')
